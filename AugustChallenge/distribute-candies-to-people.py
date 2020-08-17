a7b305128377a837252998c4c7a30949af5a0363
vladGriguta <griguta_vlad_marius@yaho0.com>
2020-08-17 22:34:16 +0100

    new sol

---
+++ AugustChallenge/distribute-candies-to-people.py
@@ -0,0 +1,15 @@
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        output=[0]*num_people
+        i=1
+        c=candies
+        
+        while i<c:
+            output[(i-1)%num_people]+=i 
+            c-=i
+            i+=1
+            
+        if c>0:
+            output[(i-1)%num_people]+=c
+
+        return output
